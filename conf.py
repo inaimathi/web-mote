daf9742d9885ba6525a41a33cc7b751f6eb1f318
Sam Mason <sam@samason.uk>
2015-02-09 21:30:54 +0000

    move port for http server out into config.py

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
 ## list of directories which contain things we might want to play
 root = ["/home/inaimathi/videos",
         "/home/inaimathi/music"]
+## what port do you want the HTTP server to listen on
+httpport = 8080
--- main.py
+++ main.py
@@ -54,5 +54,5 @@ settings = {
 app = tornado.web.Application(urls, **settings)
 
 if __name__ == "__main__":
-    app.listen(8080)
+    app.listen(httpport)
     tornado.ioloop.IOLoop.instance().start()
